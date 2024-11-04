8d49e96c5544a03adf76f5809d7035641cece764
Nifal Nizar <nifal.nizar@hotmail.com>
2024-11-04 16:30:12 +0530

    Required Validators

--- CDN/Requests/UserSaveRequest.cs
+++ CDN/Requests/UserSaveRequest.cs
@@ -1,13 +1,20 @@
-﻿namespace CDN.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CDN.Requests;
 public class UserSaveRequest
 {
+    [Required(ErrorMessage = "Username is required")]
     public string Username { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "EmailAddress is required")]
     public string EmailAddress { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "MobileNo is required")]
     public string MobileNo { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Skills is required")]
     public string Skills { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "Hobby is required")]
     public string Hobby { get; set; } = string.Empty;
 }
