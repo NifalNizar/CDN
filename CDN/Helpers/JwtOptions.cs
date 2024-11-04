4cbad7a24b1e9538241bd1670bf15a9cf39e79c1
Nifal Nizar <nifal.nizar@hotmail.com>
2024-11-04 19:54:21 +0530

    JwtOptions Added

---
+++ CDN/Helpers/JwtOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace CDN.Helpers;
+public class JwtOptions
+{
+    public const string Key = "JWT";
+
+    public string Issuer { get; set; } = string.Empty;
+
+    public string Audience { get; set; } = string.Empty;
+
+    public string Secret { get; set; } = string.Empty;
+
+    public int Duration { get; set; }
+}
